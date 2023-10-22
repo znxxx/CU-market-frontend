8bc73956e388105bf8f45481f5df32875e509d3d
znxxx <znxxx10@gmail.com>
2023-10-23 04:27:00 +0700

    Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,9 +23,9 @@ function Home(props: any) {
         <div className="left-boxes">
           <div
             id="today's picks"
-            className="shadow-[8px_8px_12px_0px_rgba(32,41,143,0.25),8px_8px_20px_0px_rgba(32,41,143,0.25)_inset] bg-[#6A5AFA] self-stretch flex flex-col px-5 lg:py-0.25 2xl:py-1 rounded-3xl max-md:max-w-full ml-16 max-md:ml-0 lg:ml-22 2xl:ml-20 mb-2"
+            className="shadow-[8px_8px_12px_0px_rgba(32,41,143,0.25),8px_8px_20px_0px_rgba(32,41,143,0.25)_inset] bg-[#6A5AFA] self-stretch flex flex-col px-5 lg:py-0.25 2xl:py-1 rounded-3xl max-md:max-w-full ml-16 max-md:ml-0 lg:ml-22 2xl:ml-20 mb-2 mr-3"
           >
-            <div className="self-center flex w-[431px] max-w-full items-center flex-1 justify-center">
+            <div className="self-center flex w-[430-px] max-w-full items-center flex-1 justify-center">
               <div className="text-stone-100 text-center 2xl:text-[28px] lg:text-[25px] font-bold">
                 TODAY’S PICKS
               </div>
@@ -52,13 +52,13 @@ function Home(props: any) {
             className="shadow-[8px_8px_12px_0px_rgba(32,41,143,0.25),8px_8px_20px_0px_rgba(32,41,143,0.25)_inset] bg-indigo-500 flex flex-col justify-center item-center rounded-3xl max-md:max-w-full max-md:ml-0 md:mr-10 max-md:mr-30 lg:mr-24 lg:ml-12 2xl:mr-28 2xl:ml-20 mb-2"
           >
             <div className="self-center flex max-w-[420px] justify-center items-center">
-              <div className="text-stone-100 text-center 2xl:text-[28px] lg:text-[25px] font-bold">
+              <div className="text-stone-100 text-center 2xl:text-[28px] lg:text-[25px] font-bold 2xl:py-2">
                 LAST MINUTE BIDS
               </div>
               <Image
                 src="/images/icons/Fire.svg"
                 alt="star"
-                className="ml-3 self-center"
+                className="ml-3 2xl:mt-1"
                 width={50}
                 height={50}
               />
